1761fbf0d4c9f7f333bc146c0f3aa0b958a5d19c
Jc Dior <jessicaclairdior@gmail.com>
2020-10-28 22:39:07 +0000

    Automatically backed up by Learn

--- lib/players/computer.rb
+++ lib/players/computer.rb
@@ -5,23 +5,23 @@ module Players
     attr_accessor :board, :game
 
     def move(board)
-        if board.valid_move?("5")
+        if @board.valid_move?("5")
           "5"
-        elsif board.valid_move?("3")
+        elsif @board.valid_move?("3")
           "3"
-        elsif board.valid_move?("1")
+        elsif @board.valid_move?("1")
           "1"
-        elsif board.valid_move?("7")
+        elsif @board.valid_move?("7")
           "7"
-        elsif board.valid_move?("9")
+        elsif @board.valid_move?("9")
           "9"
-        elsif board.valid_move?("4")
+        elsif @board.valid_move?("4")
           "4"
-        elsif board.valid_move?("2")
+        elsif @board.valid_move?("2")
           "2"
-        elsif board.valid_move?("6")
+        elsif @board.valid_move?("6")
           "6"
-        elsif board.valid_move?("8")
+        elsif @board.valid_move?("8")
           "8"
       end
     end
