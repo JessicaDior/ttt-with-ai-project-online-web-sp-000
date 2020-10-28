6c6404df712eb2a369e73df6a8171ba3a9c6eddd
Jc Dior <jessicaclairdior@gmail.com>
2020-10-28 23:11:32 +0000

    Automatically backed up by Learn

--- lib/players/computer.rb
+++ lib/players/computer.rb
@@ -7,21 +7,21 @@ module Players
     def move(board)
        if board.valid_move?("5") 
         return "5"
-      elsif board.valid_move?("3") 
+      elsif board.valid_move?("3")
         return "3"
-      elsif board.valid_move?("1") 
+      elsif board.valid_move?("1")
         return "1"
-      elsif board.valid_move?("7") 
+      elsif board.valid_move?("7")
         return "7"
-      elsif board.valid_move?("9") 
+      elsif board.valid_move?("9")
         return "9"
-      elsif board.valid_move?("4") 
+      elsif board.valid_move?("4")
         return "4"
-      elsif board.valid_move?("2") 
+      elsif board.valid_move?("2")
         return "2"
-      elsif board.valid_move?("6") 
+      elsif board.valid_move?("6")
         return "6"
-      elsif board.valid_move?("8") 
+      elsif board.valid_move?("8")
         return "8"
       end
     end
