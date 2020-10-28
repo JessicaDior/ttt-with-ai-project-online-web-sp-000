87d6b141c06727fb68dadf98161a8c0dfb24bf68
Jc Dior <jessicaclairdior@gmail.com>
2020-10-28 22:33:02 +0000

    Automatically backed up by Learn

---
+++ lib/players/computer.rb
@@ -0,0 +1,30 @@
+require_relative '../player'
+require_relative '../board'
+module Players
+  class Computer < Player
+    attr_accessor :board, :game
+
+    def move(board)
+        if board.taken?(5)
+          "5"
+        elsif board.taken?(3)
+          "3"
+        elsif board.taken?(1)
+          "1"
+        elsif board.taken?(7)
+          "7"
+        elsif board.taken?(9)
+          "9"
+        elsif board.taken?(4)
+          "4"
+        elsif board.taken?(2)
+          "2"
+        elsif board.taken?(6)
+          "6"
+        elsif board.taken?(8)
+          "8"
+      end
+    end
+  end
+
+end
