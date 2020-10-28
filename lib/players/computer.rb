dc4d63502ecede32fb7903c0bf735661a41ad17d
Jc Dior <jessicaclairdior@gmail.com>
2020-10-28 22:35:58 +0000

    Automatically backed up by Learn

--- lib/players/computer.rb
+++ lib/players/computer.rb
@@ -4,7 +4,7 @@ module Players
   class Computer < Player
     attr_accessor :board, :game
 
-    def move(board)
+    def move(board, valid_move)
         if board.valid_move?(5)
           "5"
         elsif board.valid_move?(3)
